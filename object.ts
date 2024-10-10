08f060bb336a37a5ddb6bd9c5398e9484cc15461
Anna Adamchuk <Anna Adamchuk>
2024-10-10 17:15:40 +0300

    add:file object.ts

---
+++ object.ts
@@ -0,0 +1,12 @@
+const informationAboutTheCar = {
+    salon: "темный",
+    securitysystems: "ABC",
+    pillows: "front",
+    assistanceSystems: "rain sensor",
+    exterior: "alloy wheels",
+
+}
+
+informationAboutTheCar.salon="light"
+
+console.log(informationAboutTheCar)
